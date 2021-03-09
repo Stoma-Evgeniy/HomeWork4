d9ca8c0a3a4203c4a12d79a0d092d9cb6b12e849
Евгений S <vv-bart-vv@tut.by>
2021-03-10 00:52:01 +0300

    Добавьте файлы проекта.

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HomeWork4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите положительное число");
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int j = 2; j < n; j++)
+            {
+                bool isPrime = IsPrime(j);
+
+                if (isPrime)
+                {
+                    Console.Write(j + " ");
+                }
+
+            }
+
+
+        }
+        private static bool IsPrime(int n)
+
+        {
+            bool isPrime = true;
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            return isPrime;
+        }
+    }
+}
